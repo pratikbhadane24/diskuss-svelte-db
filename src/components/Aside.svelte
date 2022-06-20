7bfa3c54f1f295e1d226bbc84311733b2592dbd3
pratikbhadane24 <pratikbhadane24@gmail.com>
2022-06-21 00:46:22 +0530

    starter setup of app

---
+++ src/components/Aside.svelte
@@ -0,0 +1,13 @@
+<aside>Diskuss is a tweet client built on Svelte. Nothing much going on over here.</aside>
+
+<style>
+  aside {
+    flex-grow: 1;
+    flex-basis: 100px;
+    padding: 20px;
+    font-size: smaller;
+    margin-top: 20px;
+    font-weight: 100;
+    opacity: 0.5;
+  }
+</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,35 @@
+<nav>
+  <div class="container">
+    <span class="logo"><i class="fa-solid fa-location-arrow" />Diskuss</span>
+  </div>
+</nav>
+
+<style>
+  .logo {
+    margin-left: 30px;
+    font-family: "Source Sans Pro";
+    font-weight: 900;
+    opacity: 0.7;
+  }
+
+  .logo i {
+    margin-right: 10px;
+  }
+
+  nav .container {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  nav .container ul {
+    display: flex;
+    height: 100%;
+  }
+
+  nav .container ul li {
+    margin: 10px;
+    list-style: none;
+  }
+    </style>
